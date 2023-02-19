3bbd3b74accc616e68d33fc4910d732f08023032
Abir Khan <abirkhan.frontend.dev24@gmail.com>
2023-02-20 00:26:16 +0600

    Stored necessary elements in variable

---
+++ main.js
@@ -0,0 +1,11 @@
+const optionBtn = document.querySelectorAll('option-button');
+const advBtn = document.querySelectorAll('adv-option-button');
+const fontName = document.getElementById('fontName');
+const fontSizeRef = document.getElementById('fontSize');
+const writingArea = document.getElementById('text-input');
+const linkButton = document.getElementById("createLink");
+const alignButtons = document.querySelectorAll(".align");
+const spacingButtons = document.querySelectorAll(".spacing");
+const formatButtons = document.querySelectorAll(".format");
+const scriptButtons = document.querySelectorAll(".script");
+
